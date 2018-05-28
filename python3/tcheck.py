402bc211bf8876947d1229dec6367d0f80e9e7eb
Muzaffer YILDIRIM <muzaffer@yldrmzffr.com>
2018-05-28 16:46:29 +0300

    Update tcheck.py

--- python3/tcheck.py
+++ python3/tcheck.py
@@ -1,6 +1,5 @@
-def check(tcno):
-	if((len(str(tcno))==11)&int(tcno[0])!=0):
-		tc = tcno
+def check(tc):
+	if((len(str(tc))==11)&int(tc[0])!=0):
 		cift = (int(tc[1]) + int(tc[3]) + int(tc[5]) + int(tc[7]))
 		tek = (int(tc[0]) + int(tc[2]) + int(tc[4]) + int(tc[6]) + int(tc[8]))
 		t10 = ((tek*7)-cift)%10
