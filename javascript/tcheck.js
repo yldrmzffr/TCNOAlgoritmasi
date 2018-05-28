5061650d1a3f531adb81f8469b8043e78478a0bb
Muzaffer YILDIRIM <muzaffer@yldrmzffr.com>
2018-05-28 17:19:27 +0300

    Update tcheck.js

--- javascript/tcheck.js
+++ javascript/tcheck.js
@@ -1,1 +1,14 @@
-
+function check(tc){
+  if(tc.length==11 && tc[0] != "0"){
+    var tek = Number(tc[0]) + Number(tc[2]) + Number(tc[4])+ Number(tc[6])+ Number(tc[8]);
+    var cift = Number(tc[1]) + Number(tc[3]) + Number(tc[5]) + Number(tc[7]);
+    var t10 = ((tek*7)-cift)%10;
+    if(Number(tc[9])==t10 && Number(tc[10])==(cift+tek+t10)%10){
+      return true;
+    }else{
+      return false;
+    }
+  }else {
+    return false;
+  }
+}
